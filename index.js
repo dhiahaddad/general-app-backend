632f8b4c5aaf81709e7bbdd7e048506466ca809b
dhiahaddad <dhiahad@gmail.com>
2025-04-06 17:29:44 +0200

    notification for new listings

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const sendPushNotification = async (content, deviceToken) => {
     sound: 'default',
     title: content.title,
     body: content.body,
+    data: content.data,
   };
+  console.log('Sending push notification:', message);
 
   try {
     const response = await fetch('https://exp.host/--/api/v2/push/send', {
@@ -32,7 +34,7 @@ const sendPushNotification = async (content, deviceToken) => {
 // Listen for changes in the 'messages' table
 // and send a push notification to the receiver when a new message is inserted
 const channel = supabase
-  .channel('realtime-push')
+  .channel('messages-listener')
   .on(
     'postgres_changes',
     { event: '*', schema: 'public', table: 'messages' },
@@ -55,8 +57,59 @@ const channel = supabase
         const notificationContent = {
           title: `Nouveau message de la part de ${receiverData.first_name} ${receiverData.last_name}`,
           body: payload.new.content,
+          data: {
+            type: 'message',
+            sender_id: payload.new.sender_id,
+          },
         };
         await sendPushNotification(notificationContent, receiver_token);
+      }
+    }
+  )
+  .subscribe();
+
+// Listen for changes in the 'messages' table
+// and send a push notification to the receiver when a new message is inserted
+const channel2 = supabase
+  .channel('requests-listener')
+  .on(
+    'postgres_changes',
+    { event: '*', schema: 'public', table: 'donation_requests' },
+    async (payload) => {
+      console.log('Change detected:', payload);
+
+      const city = payload.new.city;
+      const blood_type = payload.new.blood_type;
+      const requester_id = payload.new.requester_id;
+      const request_id = payload.new.id;
+
+      const { data: matchingUsers, error: matchingUsersError } = await supabase
+        .from('users')
+        .select('uid, device_token')
+        .eq('city', city)
+        .eq('blood_type', blood_type)
+        .neq('uid', requester_id);
+
+      if (matchingUsersError) {
+        console.error('Error fetching matching users:', matchingUsersError);
+        return;
+      }
+
+      if (!matchingUsers || matchingUsers.length === 0) {
+        console.log('No matching users found for the given city and blood type.');
+        return;
+      }
+      console.log('Matching users:', matchingUsers);
+      for (const user of matchingUsers) {
+        const notificationContent = {
+          title: 'Nouvelle demande de don de sang',
+          body: `Une demande de don de sang pour le groupe ${blood_type} a été faite dans votre région (${city}).`,
+          data: {
+            type: 'request',
+            request_id,
+          },
+        };
+        await sendPushNotification(notificationContent, user.device_token);
       }
     }
   )
